b2ab5adfaa30692c591f406460b2bed718171ca2
Nurbek <nurbekizbassar@gmail.com>
2019-08-27 19:02:03 +0600

    React-router wrappings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import RestaurantListPage from "./modules/Restaurant/RestaurantListPage";
+import RestaurantDetailsPage from "./modules/Restaurant/RestaurantDetailsPage";
+import {Switch, Route} from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <RestaurantListPage />
+      <Switch>
+        <Route exact path='/' component={RestaurantListPage} />
+        <Route exact path='/restaurants/:name' component={RestaurantDetailsPage} />
+      </Switch>
     </div>
   );
 }
