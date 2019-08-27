10a6f41230cf2b83dd7dc9f634fa1da60cd359ed
Nurbek <nurbekizbassar@gmail.com>
2019-08-27 09:36:23 +0600

    cleanup cypress examples

--- cypress/integration/smoke.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Smoke test", () => {
-  it("makes sure welcome message comes up!", () => {
-    // eslint-disable-next-line no-undef
-    cy.visit("http://localhost:3000").contains("Hello, world");
-  });
-});
